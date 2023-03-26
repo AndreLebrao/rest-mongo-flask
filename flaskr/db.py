046e6549647274571020589188a3d2add65428c4
Andre Lebebrao <andledrao@gmail.com>
2023-03-26 18:56:53 -0300

    wip : database class

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,16 +1,24 @@
-import datetime   # This will be needed later
 import os
 
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
-# Load config from a .env file:
 load_dotenv()
-MONGODB_URI = os.environ['MONGODB_URI']
 
-# Connect to your MongoDB cluster:
-client = MongoClient(MONGODB_URI)
+class Database:
+    connection_string = ""
+    db = None
+    users = None
+    
 
-# List all the databases in the cluster:
-for db_info in client.list_database_names():
-   print(db_info)
+
+def get_user_collection():
+    try:
+        client = MongoClient(MONGODB_URI)
+    except:
+        raise Exception(f"Couldn't connect to mongoDB in the following address: {MONGODB_URI}")
+    flask_db = client["flask"]
+    return flask_db["user"]
+
+user_result = user_collection.insert_one({"name":"John Doe","password":"secret"})
+user_id = user_result.inserted_id
