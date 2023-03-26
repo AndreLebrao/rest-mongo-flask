dfde16eb7df8b91668e21ef820c46ee8b770578b
Andre Lebebrao <andledrao@gmail.com>
2023-03-26 19:43:18 -0300

    style : simple description

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,7 +4,7 @@ from flask_restx import Api, Resource
 def create_app():
     # create and configure the app
     app = Flask(__name__)
-    api = Api(app)
+    api = Api(app, version="1.0", title="rest-mongo-flask",description="A simple application to get forecast data for the next 4 days from a city, given a CEP")
     
     @api.route("/health")
     class Health(Resource):
